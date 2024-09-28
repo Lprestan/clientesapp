29023b62cc6b66481de607024be12563e2028634
larryprestan <larry.prestan@gmail.com>
2024-09-28 18:00:40 -0500

    entorno de trabajo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NuevoclienteComponent } from './nuevocliente/nuevocliente.component';
+import { ListadoclientesComponent } from './listadoclientes/listadoclientes.component';
+import { EditarclienteComponent } from './editarcliente/editarcliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:NuevoclienteComponent},
+  {path:'nuevocliente',component:NuevoclienteComponent},
+  {path:'listadoclientes',component:ListadoclientesComponent},
+  {path:'editarcliente/id',component:EditarclienteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
